f0066e585bfee5be4a405b16db0f2e4682d0f804
LAZ-R <crazydrawer62@gmail.com>
2024-03-20 22:53:20 +0100

    v0.0.3 : no manifest

--- properties.js
+++ properties.js
@@ -1,3 +1,3 @@
 export const APP_NAME = 'laz_R PWA Store';
-export const APP_VERSION = '0.0.2'; // v 0.0.2 : fix logo
+export const APP_VERSION = '0.0.3'; // v0.0.3 : no manifest
 export const APP_SHORT_NAME = `lzrpwastore001`;
