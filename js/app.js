6804b673ffc5da85fb0f2597dec2227725d1259d
LAZ-R <crazydrawer62@gmail.com>
2024-03-20 20:35:27 +0100

    v 0.0.1 : init

---
+++ js/app.js
@@ -0,0 +1,95 @@
+import { setHTMLTitle } from "./utils/utils.js";
+import { APP_NAME, APP_VERSION } from "../properties.js";
+import { CATEGORIES, PWAs } from "./data.js";
+
+/* ############################################################################
+--------------------------------- CONSTANTES ---------------------------------
+############################################################################ */
+
+
+/* ############################################################################
+---------------------------------- FONCTIONS ----------------------------------
+############################################################################ */
+
+// INTERACTIONS UTILISATEUR -------------------------------
+const goTo = (link) => {
+  window.location = link;
+}
+window.goTo = goTo;
+
+// GÉNÉRATION DOM -----------------------------------------
+
+const getPwaBloc = (pwa) => {
+  return `
+    <button class="pwa-bloc" onclick="goTo('${pwa.link}')">
+      <div class="pwa-img">
+        <img src="./medias/images/pwas/${pwa.icon}.png" />
+      </div>
+      <div class="pwa-infos">
+        <h2>${pwa.name}</h2>
+        <span>${pwa.desc}</span>
+        <div>
+          <div class="dot ${
+            pwa.category == 'recommended' 
+            || pwa.category == 'tools' 
+            || pwa.category == 'games' 
+              ? 'available' 
+              : pwa.category == 'beta'
+                ? 'limited'
+                : 'unavailable'}"></div>
+          <span>${
+            pwa.category == 'recommended' 
+            || pwa.category == 'tools' 
+            || pwa.category == 'games' 
+              ? 'fonctionnel' 
+              : pwa.category == 'beta'
+                ? 'limité'
+                : 'non-fonctionnel'}</span>
+        </div>
+      </div>
+    </button>
+  `;
+}
+
+const getPwasBlocs = (pwas) => {
+  let txt = '';
+  pwas.forEach(pwa => {
+    txt += getPwaBloc(pwa);
+  });
+  return txt;
+}
+
+const getCategoryContainer = (category) => {
+  const groupPwas = PWAs.filter((element) => element.category == category.id);
+  return `
+    <div class="category-container">
+      <h1>${category.label}</h1>
+
+      ${getPwasBlocs(groupPwas)}
+
+    </div>
+  `;
+}
+
+const getCategoriesContainers = () => {
+  let txt = '';
+  CATEGORIES.forEach(category => {
+    txt += getCategoryContainer(category);
+  });
+  return txt;
+}
+
+/* ############################################################################
+---------------------------------- EXECUTION ----------------------------------
+############################################################################ */
+
+// Auto ---------------------------------------------------
+//await requestWakeLock();
+
+// Manuelle -----------------------------------------------
+setHTMLTitle(APP_NAME);
+document.getElementById('main').innerHTML = `
+  <img style="margin-top: 2dvh; height: 7dvh;" src="./medias/images/logo.png" />
+  <h1>PWA Store</h1>
+  ${getCategoriesContainers()}
+`;
---
+++ js/data.js
@@ -0,0 +1,153 @@
+export const CATEGORIES = [
+  { id: `recommended`, label: 'Recommandations' },
+  { id: `tools`, label: 'Outils et utilitaires' },
+  { id: `games`, label: 'Jeux' },
+  { id: `beta`, label: 'Versions bêta' },
+  { id: `early`, label: 'Accès anticipé' },
+];
+
+export const PWAs = [
+  // Recommandations --------------------------------------
+  {
+    icon: `gtaradios`,
+    name: `GTA Radios`,
+    desc: `Musique`,
+    category: `recommended`,
+    link: `https://laz-r.github.io/gta-radios-beta/`,
+  },
+  {
+    icon: `dinosaurs`,
+    name: `Dinosaurs`,
+    desc: `Pédagogique`,
+    category: `recommended`,
+    link: `https://laz-r.github.io/dinosaurs/`,
+  },
+
+  // Outils et utilitaires --------------------------------
+  /* {
+    icon: `geochronologia`,
+    name: `Geochronologia`,
+    desc: `Pédagogique`,
+    category: `tools`,
+    link: `https://laz-r.github.io/geochronologia/`,
+  }, */
+  {
+    icon: `colorhelper`,
+    name: `Color Helper`,
+    desc: `Outil graphique`,
+    category: `tools`,
+    link: `https://laz-r.github.io/color-helper/`,
+  },
+  {
+    icon: `vshelper`,
+    name: `Viewport-size Helper`,
+    desc: `Outil graphique`,
+    category: `tools`,
+    link: `https://laz-r.github.io/viewport-size-helper/`,
+  },
+  {
+    icon: `favorits`,
+    name: `Favorits`,
+    desc: `Gestion`,
+    category: `tools`,
+    link: `https://laz-r.github.io/favorits/`,
+  },
+  {
+    icon: `monochrono`,
+    name: `Mono Chrono`,
+    desc: `Chronomètre`,
+    category: `tools`,
+    link: `https://laz-r.github.io/mono-chrono/`,
+  },
+  {
+    icon: `sleepwave`,
+    name: `Sleepwave`,
+    desc: `Musique`,
+    category: `tools`,
+    link: `https://laz-r.github.io/sleepwave-dev/`,
+  },
+  
+  // Jeux -------------------------------------------------
+  {
+    icon: `bingo`,
+    name: `Bingo: Forces de l'Ordre`,
+    desc: `Bingo`,
+    category: `games`,
+    link: `https://laz-r.github.io/bingo/`,
+  },
+  {
+    icon: `soehelper`,
+    name: `Shadows of Evil Helper`,
+    desc: `Assistant`,
+    category: `games`,
+    link: `https://laz-r.github.io/soe-helper/`,
+  },
+  {
+    icon: `tmnt`,
+    name: `Tortues Ninja Punchlines`,
+    desc: `Collection`,
+    category: `games`,
+    link: `https://laz-r.github.io/tortues-ninja-punchlines/`,
+  },
+  {
+    icon: `yams`,
+    name: `Yams (spécial Matthieu)`,
+    desc: `Gestionaire`,
+    category: `games`,
+    link: `https://laz-r.github.io/yams-matthieu/`,
+  },
+  {
+    icon: `taptaptap`,
+    name: `Tap Tap Tap`,
+    desc: `Clicker`,
+    category: `games`,
+    link: `https://laz-r.github.io/taptaptap/`,
+  },
+
+  // Versions bêta ----------------------------------------
+  {
+    icon: `animalquiz`,
+    name: `Quiz Animaux`,
+    desc: `Quiz`,
+    category: `beta`,
+    link: `https://laz-r.github.io/animal-quizz-test/`,
+  },
+  {
+    icon: `knus`,
+    name: `Kitchen Nightmare (US)`,
+    desc: `Collection`,
+    category: `beta`,
+    link: `https://laz-r.github.io/kn-us/`,
+  },
+
+  // Accès anticipé ---------------------------------------
+  {
+    icon: `hermes`,
+    name: `Mission Hermès`,
+    desc: `Colonisation spatiale`,
+    category: `early`,
+    link: `https://laz-r.github.io/mission-hermes-dev-2/`,
+  },
+  {
+    icon: `laramidia`,
+    name: `Laramidia 3000`,
+    desc: `Aventure`,
+    category: `early`,
+    link: `https://laz-r.github.io/laramidia-3000-dev/`,
+  },
+  {
+    icon: `pkrnd`,
+    name: `PKRND`,
+    desc: `Combat`,
+    category: `early`,
+    link: `https://laz-r.github.io/pkrnd/`,
+  },
+  {
+    icon: `battles`,
+    name: `Battles`,
+    desc: `Combat`,
+    category: `early`,
+    link: `https://laz-r.github.io/battles-dev/`,
+  },
+];
+
